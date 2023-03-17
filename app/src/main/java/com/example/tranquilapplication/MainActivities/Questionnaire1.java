dd9da05ff362e20b34037edf4a94a7cd90f0b70a
Charuka Dharmasiri <charukaganganath@gmail.com>
2023-03-17 21:36:57 +0530

    17/03/2023

--- app/src/main/java/com/example/tranquilapplication/MainActivities/Questionnaire1.java
+++ app/src/main/java/com/example/tranquilapplication/MainActivities/Questionnaire1.java
@@ -1,16 +1,128 @@
 package com.example.tranquilapplication.MainActivities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+
+import com.example.tranquilapplication.QuestionsandAnswers.QuestionAnswers;
 import com.example.tranquilapplication.R;
 
-public class Questionnaire1 extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_questionnaire1);
+    public class Questionnaire1 extends AppCompatActivity implements View.OnClickListener{
+
+        TextView totalQuestionsTextView;
+        TextView questionTextView;
+        Button ansA, ansB, ansC, ansD;
+        Button submitBtn;
+
+        int score=0;
+        int totalQuestion = QuestionAnswers.question.length;
+        int currentQuestionIndex = 0;
+        String selectedAnswer = "";
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_questionnaire1);
+
+            totalQuestionsTextView = findViewById(R.id.total_question);
+            questionTextView = findViewById(R.id.question);
+            ansA = findViewById(R.id.answerA);
+            ansB = findViewById(R.id.answerB);
+            ansC = findViewById(R.id.answerC);
+            ansD = findViewById(R.id.answerD);
+            submitBtn = findViewById(R.id.submit_btn);
+
+            ansA.setOnClickListener(this);
+            ansB.setOnClickListener(this);
+            ansC.setOnClickListener(this);
+            ansD.setOnClickListener(this);
+            submitBtn.setOnClickListener(this);
+
+            totalQuestionsTextView.setText("Total questions : "+totalQuestion);
+
+            loadNewQuestion();
+
+
+
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            ansA.setBackgroundColor(Color.WHITE);
+            ansB.setBackgroundColor(Color.WHITE);
+            ansC.setBackgroundColor(Color.WHITE);
+            ansD.setBackgroundColor(Color.WHITE);
+
+            Button clickedButton = (Button) view;
+            if(clickedButton.getId()==R.id.submit_btn){
+                if(selectedAnswer.equals(QuestionAnswers.Answer1[currentQuestionIndex])){
+                    score = new Integer(score+3);
+                }
+                else if(selectedAnswer.equals(QuestionAnswers.Answer2[currentQuestionIndex])){
+                    score = new Integer(score +2);
+                }
+               else if(selectedAnswer.equals(QuestionAnswers.Answer3[currentQuestionIndex])){
+                    score = new Integer(score +1);
+                }
+                currentQuestionIndex++;
+                loadNewQuestion();
+
+
+            }else{
+                //choices button clicked
+                selectedAnswer  = clickedButton.getText().toString();
+                clickedButton.setBackgroundColor(Color.MAGENTA);
+
+            }
+
+        }
+
+        void loadNewQuestion(){
+
+            if(currentQuestionIndex == totalQuestion ){
+                finishQuiz();
+                return;
+            }
+
+            questionTextView.setText(QuestionAnswers.question[currentQuestionIndex]);
+            ansA.setText(QuestionAnswers.choices[currentQuestionIndex][0]);
+            ansB.setText(QuestionAnswers.choices[currentQuestionIndex][1]);
+            ansC.setText(QuestionAnswers.choices[currentQuestionIndex][2]);
+            ansD.setText(QuestionAnswers.choices[currentQuestionIndex][3]);
+
+        }
+
+        void finishQuiz(){
+            String passStatus = "";
+            if(score > totalQuestion*0.60){
+                passStatus = "Passed";
+            }else{
+                passStatus = "Failed";
+            }
+
+            new AlertDialog.Builder(this)
+                    .setTitle(passStatus)
+                    .setMessage("Score is "+ score+" out of "+ totalQuestion)
+                    .setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz() )
+                    .setCancelable(false)
+                    .show();
+
+
+        }
+
+        void restartQuiz(){
+            score = 0;
+            currentQuestionIndex =0;
+            loadNewQuestion();
+        }
+
     }
-}
---
+++ app/src/main/java/com/example/tranquilapplication/QuestionsandAnswers/QuestionAnswers.java
@@ -0,0 +1,81 @@
+package com.example.tranquilapplication.QuestionsandAnswers;
+
+
+public class QuestionAnswers {
+
+    public static String question[] ={
+            "I have been able to laugh and see the funny side of things",
+            "I have looked forward with enjoyment to things",
+            "I have blamed myself unnecessarily when things went wrong",
+            "I have been anxious or worried for no good reason",
+            "I have felt scared or panicky for no very good reason",
+            "Things have been getting on top of me",
+            "I have been so unhappy that I have had difficulty sleeping",
+            "I have felt sad or miserable",
+            "I have been so unhappy that I have been crying",
+            "The thought of harming myself has occurred to me",
+    };
+
+    public static String choices[][] = {
+            {"As much as I always could","Not quite so much now","Definitely not so much now","Not at all"},
+            {"As much as I ever did","Rather less than I used to","Definitely less than I used to","Hardly at all"},
+            {"Yes, most of the time","Yes, some of the time","Not very often","No, never"},
+            {"No, not at all","Hardly ever","Yes, sometimes","Yes, very often"},
+            {"Yes, quite a lot","Yes, sometimes","No, not much","No, not at all"},
+            {"Yes, most of the time I haven¶t been able to cope at all","Yes, sometimes I haven¶t been coping as well as usual","No, most of the time I have coped quite well","No, I have been coping as well as ever"},
+            {"Yes, most of the time","Yes, sometimes","Not very often","No, not at all"},
+            {"Yes, most of the time","Yes, quite often","Not very often","No, not at all"},
+            {"Yes, most of the time","Yes, quite often","Only occasionally","No, never"},
+            {"Yes, quite often","Sometimes","Hardly ever","Never"},
+    };
+
+    public static String Answer1[] = {
+            "As much as I always could",
+            "As much as I ever did",
+            "Yes, most of the time",
+            "No, not at all",
+            "Yes, quite a lot",
+            "Yes, most of the time I haven¶t been able to cope at all",
+            "Yes, most of the time",
+            "Yes, most of the time",
+            "Yes, most of the time",
+            "Yes, quite often"
+    };
+    public static String Answer2[] = {
+            "Not quite so much now",
+            "Rather less than I used to",
+            "Yes, some of the time",
+            "Hardly ever",
+            "Yes, sometimes",
+            "Yes, sometimes I haven¶t been coping as well as usual",
+            "Yes, sometimes",
+            "Yes, quite often",
+            "Yes, quite often",
+            "Sometimes"
+    };
+    public static String Answer3[] = {
+            "Definitely not so much now",
+            "Definitely less than I used to",
+            "Not very often",
+            "Yes, sometimes",
+            "No, not much",
+            "No, most of the time I have coped quite well",
+            "Not very often",
+            "Not very often",
+            "Only occasionally",
+            "Hardly ever"
+    };
+    public static String Answer4[] = {
+            "Not at all",
+            "Hardly at all",
+            "No, never",
+            "Yes, very often",
+            "No, not at all",
+            "No, I have been coping as well as ever",
+            "No, not at all",
+            "No, not at all",
+            "No, never",
+            "Never"
+    };
+
+}
